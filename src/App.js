6a739189db8774bb3fa063819d87118eadde4a7d
LAPT-CSH-15 <cshtd.computacion@gmail.com>
2025-10-28 14:09:37 -0600

    Navbar compleo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './paginas/Home';
+import Education from './paginas/Education';
+import Projects from './paginas/Projects';
 
 function App() {
   return (
